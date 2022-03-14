b547478eb6c85d447d9a3c529ef9f8e7cd087fec
Valarie Chelagat <valarie.chelagat@student.moringaschool.com>
2022-03-13 23:24:13 -0400

    added toggle element

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
 $(".toggler").click(function() {
     
     // Reset all
+    $(".toggler img.product").hide();
+    $(".toggler p.par").show();
+    
+    // Now toggle the ones in this .toggler
+    $("img", this).toggle();
+});
+$(".toggler").click(function() {
+    
+    // Reset all
+    $(".toggler img.development").hide();
+    $(".toggler p.par").show();
+    
+    // Now toggle the ones in this .toggler
+    $("img", this).toggle();
+});
+$(".toggler").click(function() {
+    
+    // Reset all
     $(".toggler img.design").hide();
     $(".toggler p.par").show();
     
