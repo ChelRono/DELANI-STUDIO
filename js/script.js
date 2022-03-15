9571dcc1536de22a55bf5290f3e18f0ee33b3e37
Valarie Chelagat <valarie.chelagat@student.moringaschool.com>
2022-03-14 23:29:47 -0400

    added spacing to my images

--- js/script.js
+++ js/script.js
@@ -1,11 +1,26 @@
 $(document).ready(function () {
     var $content = $(".content").hide();
-    $(".toggle").on("click", function (e) {
+    $(".toggle").on("click", function {
         $(this).toggleClass("expanded");
         $(this).next().slideToggle();
 
     });
 });
+/* $(document).ready(function(){
+    $("toggle").click(function(){
+      $("p").toggle();
+    });
+  }); */
+ /*  $(document).ready(function(){
+    $(".development").click(function(){
+      $("p").toggle();
+    });
+  });
+  $(document).ready(function(){
+    $(".product").click(function(){
+      $("p").toggle();
+    });
+  }); */
 //hover element
 $(document).ready( function() {
 
